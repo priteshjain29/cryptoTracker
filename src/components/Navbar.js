72335d627a0ed2c64ed0a6c1303afbe633d0b025
priteshjain29 <itspritesh2@gmail.com>
2023-12-16 16:08:16 +0530

    crypto tracker react app

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {FaCoins} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <Link to='/'>
+            <div className='navbar'>
+                <h1> Crypto <span className='purple'>Tracker</span></h1>
+            </div>
+        </Link>
+    )
+}
+
+export default Navbar
